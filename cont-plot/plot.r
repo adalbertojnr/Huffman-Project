5c8528fed906b146132a5f8f8a62bfa68d3f5f64
Adalberto de Lima <Adalberto@MacBook-de-Adalberto.local>
2020-01-30 16:30:00 -0300

    fifth commit (descompactar pronta)

--- cont-plot/plot.r
+++ cont-plot/plot.r
@@ -1,28 +1,28 @@
-comp208 <- read.table("plotagem.txt", header = T, sep = "\t")
+comp208 <- read.table("Comp_Data.txt", header = T, sep = "\t")
 
 #Compute the largest y and x in the data
-max_y <- max(comp208$ABB, comp208$AVL)
-max_x <- max(comp208$posicao, comp208$posicao)
+max_y <- max(comp208$Comparações_AVL, comp208$Comparações_ABB)
+max_x <- max(comp208$Tamanho_AVL, comp208$Tamanho_ABB)
 
 #Define colors to be used for AVL and BST
 plot_colors <- c("black", 'red')
 
 #Start PNG device to save output to Fig_Plot.png
-png("Fig_Plot.png", height = 720, width = 1280, bg = "white")
+png("Grafico_plotagem.png", height = 720, width = 1280, bg = "white")
 
 #Plotting
-plot(comp208$posicao, comp208$AVL, type = "p", pch = 20, col = plot_colors[1],
+plot(comp208$Tamanho_ABB, comp208$Comparações_ABB, type = "p", pch = 20, col = plot_colors[1],
      ylim = c(0, max_y), xlim = c(0, max_x), axes = FALSE, ann = FALSE)
 
 #Change interval scale
-axis(1, las = 1, at = ((max_x %% 1000000)/10)*0:max_x)
+axis(1, las = 1, at = ((max_x %% 100000)/10)*0:max_x)
 
-axis(2, las = 1, at = ((max_y %% 1000000)/10)*0:max_y)
+axis(2, las = 1, at = ((max_y %% 100000)/10)*0:max_y)
 
 box()
 
 #Graph BST
-lines(comp208$posicao, comp208$ABB, type = "p", pch = 20, lty = 2, col = plot_colors[2])
+lines(comp208$Tamanho_AVL, comp208$Comparações_AVL, type = "p", pch = 20, lty = 2, col = plot_colors[2])
 
 #Create a title with red, bold/italic font
 title(main = "Search Algorithm: AVL x BST", col.main = "black", font.main = 4)
